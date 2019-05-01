b1d90ef0ac09f8e7af8c898e768d9a78f6649c4e
Tyler Hutcherson <tyler.hutcherson@metismachine.comn>
2019-05-01 11:29:50 -0400

    add setup requirements

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+DESCRIPTION = """"""
+
+REQS = """"""
+
+VERSION = """"""
+
+setup(
+  name='skafossdk',
+  packages=['skafossdk'],
+  version=VERSION,
+  license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description=DESCRIPTION,
+  author='Skafos, LLC',
+  author_email='skafos@skafos.ai',
+  url='https://github.com/skafos/skafossdk',
+  download_url='',
+  keywords=['machine learning delivery', 'mobile deployment', 'model versioning'],
+  install_requires=REQS,
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)

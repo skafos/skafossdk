647c007c390109b9bf01a9c3ca0c47fd24d95182
Tyler Hutcherson <tyler.hutcherson@metismachine.comn>
2019-06-11 14:17:48 -0400

    Add a few more tests

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2,6 +2,7 @@ import pytest
 import skafos
 from skafos import models
 from skafos.exceptions import *
+from skafos.http import generate_required_params
 
 from constants import *
 
@@ -12,23 +13,23 @@ PARAMS = {
 }
 
 class TestExceptions(object):
-    # Test a token error
-    def test_missing_token(self):
+    # Test a token error on a summary call with no token
+    def test_summary_missing_token(self):
         with pytest.raises(InvalidTokenError):
             skafos.summary()
 
-    # Test another token error
-    def test_invalid_token(self):
+    # Test another token error by using a fake token
+    def test_summary_invalid_token(self):
         with pytest.raises(InvalidTokenError):
             skafos.summary(skafos_api_token=TESTING_FAKE_TOKEN)
 
-    # Test missing param error
-    def test_missing_param(self):
+    # Test missing param error when trying to upload a file
+    def test_upload_missing_param(self):
         with pytest.raises(InvalidParamError):
             models.upload_version(files="", skafos_api_token=TESTING_FAKE_TOKEN)
 
     # Test a failing download due to empty model version
-    def test_failed_fetch(self):
+    def test_fetch_failed_download(self):
         with pytest.raises(DownloadFailedError):
             res = models.fetch_version(
                 model_name=TESTING_MODEL_EMPTY,
@@ -36,10 +37,16 @@ class TestExceptions(object):
             )
 
     # Test a failed upload due to invalid (fake) file
-    def test_invalid_file_upload(self):
+    def test_upload_invalid_file(self):
         with pytest.raises(InvalidParamError):
             res = models.upload_version(
                 files=TESTING_FAKE_FILE,
                 model_name=TESTING_MODEL,
                 **PARAMS
             )
+
+    # Test generating required PARAMS when missing a value
+    def test_generate_params_missing(self):
+        with pytest.raises(InvalidParamError):
+            # Missing model name here.. should fail
+            res = generate_required_params(PARAMS)
--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,6 +1,6 @@
 import skafos
 from skafos import models
-
+from skafos.http import generate_required_params
 from constants import *
 
 PARAMS = {
@@ -14,6 +14,13 @@ class TestMethods(object):
     def test_version(self):
         v = skafos.get_version()
         assert isinstance(v, str)
+
+    # Test generating required PARAMS
+    def test_generate_params(self):
+        required_keys = ["app_name", "org_name", "app_name", "model_name"]
+        res = generate_required_params({**PARAMS, **{"model_name": "test_model"}})
+        # Check for required keys
+        assert all(key in res.keys() for key in required_keys)
 
     def test_compact_summary(self):
         response = skafos.summary(skafos_api_token=TESTING_API_TOKEN, compact=True)
@@ -47,4 +54,3 @@ class TestMethods(object):
         # Check response type and verfiy that nothing comes back
         assert isinstance(res, list)
         assert len(res) == 0
-        
